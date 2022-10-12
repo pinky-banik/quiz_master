2fba0e57b79485bb927dcb83ba3868def1fb9cda
pinky-banik <pinky.rani.banik@gmail.com>
2022-10-12 14:54:50 +0600

    project setup

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'banner': "",    
+      }
+    },
+  },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#3A4256",
+          secondary: "#0FCFEC",
+          accent: "#19D3AE",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+    ],
+  },
+  plugins: [require("daisyui")],
+}
