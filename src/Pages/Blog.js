2158ce8cdb942b79dd85638cc838d38443f22e24
pinky-banik <pinky.rani.banik@gmail.com>
2022-10-12 20:21:27 +0600

    blog page done

--- src/Pages/Blog.js
+++ src/Pages/Blog.js
@@ -3,7 +3,18 @@ import React from 'react';
 const Blog = () => {
   return (
     <div>
-      
+      <div>
+        <h1>What is the purpose of react router?</h1>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis obcaecati commodi itaque ab unde, ullam dolore voluptates soluta assumenda adipisci qui facilis excepturi? Adipisci deserunt odio dicta sit quia voluptate distinctio, modi iure totam hic expedita veritatis quibusdam quos quod tenetur sunt nostrum inventore praesentium ea delectus, cum est corrupti?</p>
+      </div>
+      <div>
+        <h1>How does Context API works?</h1>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis obcaecati commodi itaque ab unde, ullam dolore voluptates soluta assumenda adipisci qui facilis excepturi? Adipisci deserunt odio dicta sit quia voluptate distinctio, modi iure totam hic expedita veritatis quibusdam quos quod tenetur sunt nostrum inventore praesentium ea delectus, cum est corrupti?</p>
+      </div>
+      <div>
+        <h1>Tell Us about useRef hook.</h1>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis obcaecati commodi itaque ab unde, ullam dolore voluptates soluta assumenda adipisci qui facilis excepturi? Adipisci deserunt odio dicta sit quia voluptate distinctio, modi iure totam hic expedita veritatis quibusdam quos quod tenetur sunt nostrum inventore praesentium ea delectus, cum est corrupti?</p>
+      </div>
     </div>
   );
 };
